11bbbd72acfddcd6fc74ee159109b4868a0c1ece
HarveyShan <harveyshan918@gmail.com>
2018-06-14 20:14:42 -0700

    Finished String Manipulation

--- BasicClass/src/StringManipulation.java
+++ BasicClass/src/StringManipulation.java
@@ -2,15 +2,21 @@ import java.util.Scanner;
 public class StringManipulation {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in); // system.in is input, it captures the variable the input value of the user 
+		Scanner input = new Scanner(System.in);// system.in is input, it captures the variable the input value of the user 
 		
 		System.out.print("Enter your full name: ");
 		
-		String userName = input.nextLine();  // regulates the type of variable the user must input 
+		String userName = input.nextLine();// regulates the type of variable the user must input 
 		
-		System.out.println(userName.substring(0, 1)); // system.out is output 
+		System.out.println(userName.substring(0, 1));// system.out is output 
 		String[] parts = userName.split(" ");
-		System.out.println(parts.toString());
+		String firstName = parts[0];
+//		String lastName = parts[1];
+		System.out.println(firstName);
+		int firstSpace = userName.lastIndexOf(" ");
 		
+		System.out.println(userName.substring(firstName.length()+1));
+		
+		System.out.println(userName.substring(4, firstSpace));
 		}
 }
